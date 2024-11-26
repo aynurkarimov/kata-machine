771bf8dfc2e03b0ded335297bfc45707224d792b
Aynur Karimov <hey@aynurkarimov.com>
2024-11-26 20:42:20 +0400

    Bubble like everyone does

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,22 +1,36 @@
+// export default function bubble_sort(arr: number[]): void {
+//     let i = 0;
+//     let high = arr.length;
+
+//     do {
+//         if (i - 1 === high) {
+//             i = 0;
+//             high -= 1;
+
+//             continue;
+//         }
+
+//         if (arr[i] > arr[i + 1]) {
+//             const bigger = arr[i];
+
+//             arr[i] = arr[i + 1];
+//             arr[i + 1] = bigger;
+//         }
+
+//         i++;
+//     } while (high !== 1);
+// }
+//
+
 export default function bubble_sort(arr: number[]): void {
-    let i = 0;
-    let high = arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const bigger = arr[j];
 
-    do {
-        if (i - 1 === high) {
-            i = 0;
-            high -= 1;
-
-            continue;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = bigger;
+            }
         }
-
-        if (arr[i] > arr[i + 1]) {
-            const bigger = arr[i];
-
-            arr[i] = arr[i + 1];
-            arr[i + 1] = bigger;
-        }
-
-        i++;
-    } while (high !== 1);
+    }
 }
