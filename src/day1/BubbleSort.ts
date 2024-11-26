8f539795ed435330f6816a6edfb81250eaf07efe
Aynur Karimov <hey@aynurkarimov.com>
2024-11-26 20:38:14 +0400

    Unorthodox implementation of bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,22 @@
 export default function bubble_sort(arr: number[]): void {
+    let i = 0;
+    let high = arr.length;
 
+    do {
+        if (i - 1 === high) {
+            i = 0;
+            high -= 1;
+
+            continue;
+        }
+
+        if (arr[i] > arr[i + 1]) {
+            const bigger = arr[i];
+
+            arr[i] = arr[i + 1];
+            arr[i + 1] = bigger;
+        }
+
+        i++;
+    } while (high !== 1);
 }
