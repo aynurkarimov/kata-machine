5617c46ea3fbdf20a5ca168e36426a095efe93bf
Aynur Karimov <hey@aynurkarimov.com>
2024-11-26 20:23:45 +0400

    Add already solved problems

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let high = haystack.length;
+    let low = 0;
 
+    while (high > low) {
+        let mid = Math.floor(low + (high - low) / 2);
+        let value = haystack[mid];
+
+        if (value === needle) {
+            return true;
+        } else if (needle > value) {
+            low = mid + 1;
+        } else if (needle < value) {
+            high = mid;
+        }
+    }
+
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const sqrt = Math.floor(Math.sqrt(breaks.length));
+    let step = 1;
+    let index = -1;
 
+    while (sqrt * step < breaks.length && index === -1) {
+        if (breaks[sqrt * step] === true) {
+            for (let i = sqrt * (step - 1); i <= sqrt * step; i++) {
+                if (breaks[i] === true) {
+                    index = i;
+                    break;
+                }
+            }
+        } else {
+            step += 1;
+        }
+    }
+
+    return index;
 }
